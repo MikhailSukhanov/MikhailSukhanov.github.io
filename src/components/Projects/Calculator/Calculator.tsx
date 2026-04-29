9fef1be7d6ce8c24693a9ee438d342e7e1728b1e
Mikhail Sukhanov <MikhailSukhanov55@yandex.ru>
2026-04-29 15:49:42 +0300

    refactor(Calculator): make buttons display via map function perf(Calculator): memoize calculation results fix(Calculator): fix input padding

--- src/components/Projects/Calculator/Calculator.tsx
+++ src/components/Projects/Calculator/Calculator.tsx
@@ -1,10 +1,17 @@
 import { evaluate, round } from 'mathjs';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
+import { type PayloadAction } from '@reduxjs/toolkit';
 import styles from './calculator.module.scss';
 import { setMathExp, addCharToMathExp } from '../../../store/slices';
 import { useDirectProjectLink } from '../../../store/projects.ts';
 import { useAppDispatch, useAppSelector } from '../../../store/store.ts';
+
+interface IButton {
+    label: string,
+    action: () => PayloadAction<string>,
+    type?: string
+}
 
 function Calculator() {
     const location = useLocation();
@@ -29,38 +36,40 @@ function Calculator() {
         }
     }
 
-    const result = evalMathExp(mathExpression);
+    const result = useMemo(() => evalMathExp(mathExpression), [mathExpression]);
+    const buttons: IButton[] = [
+        {label: 'AC', action: () => setMathExp(''), type: 'operator'},
+        {label: '(', action: () => addCharToMathExp('('), type: 'operator'},
+        {label: ')', action: () => addCharToMathExp(')'), type: 'operator'},
+        {label: '⇦', action: () => setMathExp(mathExpression.slice(0, -1)), type: 'operator'},
+        {label: '÷', action: () => addCharToMathExp('/'), type: 'operator'},
+        {label: 'π', action: () => addCharToMathExp('pi')},
+        {label: '7', action: () => addCharToMathExp('7')},
+        {label: '8', action: () => addCharToMathExp('8')},
+        {label: '9', action: () => addCharToMathExp('9')},
+        {label: '×', action: () => addCharToMathExp('*'), type: 'operator'},
+        {label: 'e', action: () => addCharToMathExp('e')},
+        {label: '4', action: () => addCharToMathExp('4')},
+        {label: '5', action: () => addCharToMathExp('5')},
+        {label: '6', action: () => addCharToMathExp('6')},
+        {label: '−', action: () => addCharToMathExp('-'), type: 'operator'},
+        {label: '√', action: () => addCharToMathExp('sqrt(')},
+        {label: '1', action: () => addCharToMathExp('1')},
+        {label: '2', action: () => addCharToMathExp('2')},
+        {label: '3', action: () => addCharToMathExp('3')},
+        {label: '+', action: () => addCharToMathExp('+'), type: 'operator'},
+        {label: '^', action: () => addCharToMathExp('^')},
+        {label: '00', action: () => addCharToMathExp('00')},
+        {label: '0', action: () => addCharToMathExp('0')},
+        {label: ',', action: () => addCharToMathExp('.')},
+        {label: '=', action: () => setMathExp(result), type: 'equals'}
+    ];
+    const dispButtons = buttons.map(btn => <button key={btn.label} onClick={() => dispatch(btn.action())} data-type={btn.type}>{btn.label}</button>);
 
     return <div className={styles.container}>
         <div className={styles.result}>{result === mathExpression ? '' : result}</div>
         <input onChange={e => dispatch(setMathExp(e.target.value))} value={mathExpression}/>
-        <div className={styles.buttons}>
-            <button onClick={() => dispatch(setMathExp(''))} data-type="operator">AC</button>
-            <button onClick={() => dispatch(addCharToMathExp('('))} data-type="operator">{'('}</button>
-            <button onClick={() => dispatch(addCharToMathExp(')'))} data-type="operator">{')'}</button>
-            <button onClick={() => dispatch(setMathExp(mathExpression.slice(0, -1)))} data-type="operator">⇦</button>
-            <button onClick={() => dispatch(addCharToMathExp('/'))} data-type="operator">÷</button>
-            <button onClick={() => dispatch(addCharToMathExp('pi'))}>π</button>
-            <button onClick={() => dispatch(addCharToMathExp('7'))}>7</button>
-            <button onClick={() => dispatch(addCharToMathExp('8'))}>8</button>
-            <button onClick={() => dispatch(addCharToMathExp('9'))}>9</button>
-            <button onClick={() => dispatch(addCharToMathExp('*'))} data-type="operator">×</button>
-            <button onClick={() => dispatch(addCharToMathExp('e'))}>e</button>
-            <button onClick={() => dispatch(addCharToMathExp('4'))}>4</button>
-            <button onClick={() => dispatch(addCharToMathExp('5'))}>5</button>
-            <button onClick={() => dispatch(addCharToMathExp('6'))}>6</button>
-            <button onClick={() => dispatch(addCharToMathExp('-'))} data-type="operator">−</button>
-            <button onClick={() => dispatch(addCharToMathExp('sqrt('))}>√</button>
-            <button onClick={() => dispatch(addCharToMathExp('1'))}>1</button>
-            <button onClick={() => dispatch(addCharToMathExp('2'))}>2</button>
-            <button onClick={() => dispatch(addCharToMathExp('3'))}>3</button>
-            <button onClick={() => dispatch(addCharToMathExp('+'))} data-type="operator">+</button>
-            <button onClick={() => dispatch(addCharToMathExp('^'))}>^</button>
-            <button onClick={() => dispatch(addCharToMathExp('00'))}>00</button>
-            <button onClick={() => dispatch(addCharToMathExp('0'))}>0</button>
-            <button onClick={() => dispatch(addCharToMathExp('.'))}>,</button>
-            <button onClick={() => dispatch(setMathExp(result))} data-type="equals">=</button>
-        </div>
+        <div className={styles.buttons}>{dispButtons}</div>
     </div>;
 }
 

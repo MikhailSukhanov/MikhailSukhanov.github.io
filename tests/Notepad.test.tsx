ddad71ccf168a5bad82c9c5a7013960042195782
Mikhail Sukhanov <MikhailSukhanov55@yandex.ru>
2026-05-06 21:52:39 +0300

    test(Notepad): add note selection and filtering tests. Edit rendering tests names. refactor(Notepad): refactor note editing test.

--- tests/Notepad.test.tsx
+++ tests/Notepad.test.tsx
@@ -9,7 +9,7 @@ beforeAll(() => {
 });
 
 describe('Rendering', () => {
-    it('Renders "Add" button (ru)', () => {
+    it('Renders the "Add" button (ru)', () => {
         renderWithProviders(<Notepad/>, {preloadedState: {themeAndLang: {
             language: 'ru',
             theme: 'dark'
@@ -18,7 +18,7 @@ describe('Rendering', () => {
         expect(addBtn).toBeInTheDocument();
     });
 
-    it('Renders "Add" button (en)', () => {
+    it('Renders the "Add" button (en)', () => {
         renderWithProviders(<Notepad/>, {preloadedState: {themeAndLang: {
             language: 'en',
             theme: 'dark'
@@ -27,17 +27,17 @@ describe('Rendering', () => {
         expect(addBtn).toBeInTheDocument();
     });
 
-    it('Renders search field', () => {
+    it('Renders the search field', () => {
         renderWithProviders(<Notepad/>);
         const input = screen.getByRole('textbox', {name: 'Notes searching'});
         expect(input).toBeInTheDocument();
     });
 
-    it('Renders textarea', () => {
+    it('Renders the textarea', () => {
         renderWithProviders(<Notepad/>);
         const textarea = screen.getByRole('textbox', {name: 'Note editing'});
         expect(textarea).toBeInTheDocument();
-    })
+    });
 });
 
 describe('Functionality', () => {
@@ -56,17 +56,49 @@ describe('Functionality', () => {
         expect(updatedNotes.length).toBe(1);
     });
 
+    it('Selects a note', async () => {
+        const {store} = renderWithProviders(<Notepad/>, {preloadedState: {notepad: {
+            selectedNoteId: null,
+            searchField: '',
+            notes: [{id: '1', text: ''}]
+        }}});
+
+        const note = screen.getByTestId('note-item');
+        await user.click(note);
+        const selectedNoteId = store.getState().notepad.selectedNoteId;
+        expect(selectedNoteId).toBe('1');
+    });
+
     it('Edits a note', async () => {
-        renderWithProviders(<Notepad/>);
-        const newText: string = 'First note!';
+        const initialText: string = 'Cool';
+        const newText: string = 'Tests';
 
-        const addBtn = screen.getByRole('button', {name: /(Добавить|Add)/});
-        await user.click(addBtn);
+        renderWithProviders(<Notepad/>, {preloadedState: {notepad: {
+            selectedNoteId: '1',
+            searchField: '',
+            notes: [{id: '1', text: initialText}]
+        }}});
 
+        const note = screen.getByTestId('note-item');
         const textarea = screen.getByRole('textbox', {name: 'Note editing'});
         await user.type(textarea, newText);
 
-        const newNote = screen.getByTestId('note-item');
-        expect(newNote).toHaveTextContent(newText);
+        expect(note).toHaveTextContent(initialText + newText);
+    });
+
+    it('Filters notes', async () => {
+        renderWithProviders(<Notepad/>, {preloadedState: {notepad: {
+            selectedNoteId: null,
+            searchField: '',
+            notes: [{id: '1', text: 'Cool'}, {id: '2', text: 'Tests'}]
+        }}});
+        
+        const input = screen.getByRole('textbox', {name: 'Notes searching'});
+        const initialNotes = screen.getAllByTestId('note-item');
+        expect(initialNotes.length).toBe(2);
+
+        await user.type(input, 'tests');
+        const filteredNotes = screen.queryAllByTestId('note-item');
+        expect(filteredNotes.length).toBe(1);
     });
 });
